6a7def73af4717ce59788023e3bcbad449db7ab7
nehs <1223922280@qq.com>
2020-09-04 16:22:45 +0800

    MAVEN:spring-boot-starter-actuator

--- src/main/java/com/nehs/antares/constant/CommonConstant.java
+++ src/main/java/com/nehs/antares/constant/CommonConstant.java
@@ -4,9 +4,9 @@ public interface CommonConstant {
     /**
      * 状态码200为成功
      */
-    Long succeedCode = 200l;
+    Long succeedCode = 200L;
     /**
      * 状态码300为失败
      */
-    Long failedCode = 300l;
+    Long failedCode = 300L;
 }
