3fbb4c8afdf6430083888700afc2a2bb5ec2db7a
nehs <1223922280@qq.com>
2020-10-09 13:54:40 +0800

    主启动类有@MapperScan("")注解后，在Mapper.java中就无需@Mapper注解

--- src/main/java/com/nehs/antares/mapper/employees/EmployeesMapper.java
+++ src/main/java/com/nehs/antares/mapper/employees/EmployeesMapper.java
@@ -2,12 +2,10 @@ package com.nehs.antares.mapper.employees;
 
 
 import com.nehs.antares.entity.Employees;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-@Mapper
 public interface EmployeesMapper {
     Employees getEmployeesByEmpNo(@Param("empNo") Long empNo);
 
